1015b54ae35f87b95504e5721e6db8e584ee4ce7
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-25 22:00:47 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -68,67 +68,89 @@ class ExpenseViewer{
         }
 };
 
-class SplitLogic{
-    public:
-        virtual unordered_map<string,double>split(double amt,vector<string>& participants)=0;
+// class SplitLogic{
+//     public:
+//         virtual unordered_map<string,double>split(double amt,vector<string>& participants)=0;
 
-};
+// };
 
-class EqualSplitLogic: public SplitLogic{
-    public:
-        unordered_map<string,double> split(double amt,vector<string>& participants){
-            unordered_map<string,double> result;
-            double eachshare=amt/participants.size();
+// class EqualSplitLogic: public SplitLogic{
+//     public:
+//         unordered_map<string,double> split(double amt,vector<string>& participants){
+//             unordered_map<string,double> result;
+//             double eachshare=amt/participants.size();
             
-            for(auto member:participants){
-                result[member]=eachshare;
-            }
-            return result;
-        }
-};
+//             for(auto member:participants){
+//                 result[member]=eachshare;
+//             }
+//             return result;
+//         }
+// };
 
-class BalanceData{
+// class BalanceData{
+//     private:
+//         unordered_map<string,unordered_map<string,double>> balances;
+//     public:
+//         void updateBalance(string payee,unordered_map<string,double>& splitdata){
+//             for(auto& data:splitdata){
+//                 string user=data.first;
+//                 double amt=data.second;
+
+//                 if(user==payee) continue;
+
+//                 balances[user][payee]+=amt;
+//             }
+//         }
+
+//         void showBalance(){
+//             for(auto debtor:balances){
+//                 for(auto creditor:debtor.second){
+//                     cout<< debtor.first<<" HAS TO PAY Rs."<< creditor.second<<" TO "<< creditor.first<<endl;
+//                 }
+//             }
+//         }
+
+// };
+class ExpenseManager{
     private:
+        vector<User> users;
+        vector<Expense> expenseList;
         unordered_map<string,unordered_map<string,double>> balances;
+
     public:
-        void updateBalance(string payee,unordered_map<string,double>& splitdata){
-            for(auto& data:splitdata){
-                string user=data.first;
-                double amt=data.second;
-
-                if(user==payee) continue;
-
-                balances[user][payee]+=amt;
-            }
+        void addUser(int id,string name){
+            users.push_back(User(id,name));
         }
+        void addExpense(int expid,string desc,double amt,string paidby,vector<string> participants){
 
+            if(participants.empty() || amt==0){
+                return;
+            }
+            expenseList.push_back(Expense(expid,desc,amt,paidby,participants));
+            double share=amt/participants.size();
+            for(auto mem:participants){
+                if(mem==paidby) continue;
+                balances[mem][paidby]+=share;
+            }
+
+            ofstream file("expenses.txt",ios::app);
+            file<<expid<<","<<desc<<","<<amt<<","<<paidby;
+            for(auto mem:participants){
+                file<<","<<mem;
+            }
+            file<<endl;
+            file.close();
+        }
         void showBalance(){
+            if(balances.empty()){
+                cout<<"All Settled! \n";
+                return;
+            }
             for(auto debtor:balances){
                 for(auto creditor:debtor.second){
                     cout<< debtor.first<<" HAS TO PAY Rs."<< creditor.second<<" TO "<< creditor.first<<endl;
                 }
             }
-        }
-
-};
-class ExpenseManager{
-    private:
-        vector<User> users;
-        vector<Expense> expenseList;
-        BalanceData balances;
-
-    public:
-        void addUser(User& user){
-            users.push_back(user);
-        }
-        void addExpense(Expense& exp,SplitLogic* logic){
-            auto splits=logic->split(exp.getAmount(),exp.getParticipants());
-
-            balances.updateBalance(exp.getPaidBy(),splits);
-            expenseList.push_back(exp);
-        }
-        void showBalance(){
-            balances.showBalance();
         }
         vector<Expense>& getExpenses(){
             return expenseList;
@@ -139,5 +161,65 @@ class ExpenseManager{
 };
 
 int main(){
+    ExpenseManager manager;
+    ExpenseViewer viewer;
+    // EqualSplitLogic equalSplit;
+
+    int choice;
+    while(true){
+        cout<<"\n 1. Add User \n 2. Add Expense \n 3. Show Expense \n 4. Show Balance \n 5. Exit\n";
+        cout<<"ENTER CHOICE: \n";
+        cin>>choice;
+
+        if(choice==1){
+            int id;
+            string name;
+            cout<<"Enter User ID: ";cin>>id;
+            cout<<"Enter Name: ";cin>>name;
+
+            manager.addUser(id,name);
+            cout<<"User Added Succesfully.\n";
+        }
+        else if(choice==2){
+            int expid;
+            double amt;
+            string desc,paidBy;
+            int n;
+
+            cout<<"Expense ID: ";
+            cin>>expid;
+            cout<<"Description: ";
+            cin.ignore();
+            getline(cin,desc);
+            cout<<"Total Amount: ";
+            cin>>amt;
+            cout<<"Paid By: ";
+            cin>>paidBy;
+            cout<<"Number of Participants: ";
+            cin>>n;
+
+            vector<string> participants;
+            for(int i=0;i<n;i++){
+                string name;
+                cout<<"Participant "<<i+1<<": ";
+                cin>>name;
+                participants.push_back(name);
+            }
+            manager.addExpense(expid,desc,amt,paidBy,participants);
+            cout<<"Expense Added Successfully\n";
+
+        }
+        else if(choice==3){
+            manager.viewExpenses(viewer);
+        }
+        else if(choice==4){
+            manager.showBalance();
+        }
+        else if(choice==5){
+            cout<<"Exiting Program...";
+            break;
+        }
+    }
+
     return 0;
 }
